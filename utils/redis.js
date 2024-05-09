649189b9b12e61c3c82c7c0a8ce0f760060eddaa
swiftleah <leahrosehenry@gmail.com>
2024-05-09 02:05:59 -0700

    Task 0 better but redis still not successfully connecting

--- utils/redis.js
+++ utils/redis.js
@@ -1,45 +1,37 @@
 const redis = require('redis');
 
-/**
- * creating a Redis client - displays any error
- * function isAlive - returns true when connection is successful
- * & false when not successful.
- *
- * asynchronous function get, set and del for key
- */
-
 class RedisClient {
-	constructor() {
-		this.client = redis.createClient();
+    constructor() {
+        this.client = redis.createClient();
 
-		this.client.on('error', (error) => {
-			console.error(`${error}`);
-		});
-	}
+        this.client.on('error', (error) => {
+            console.error('Error connecting to Redis:', error);
+        });
+    }
 
-	isAlive() {
-		return this.client.isReady;
-	}
+    isAlive() {
+        return this.client.connected;
+    }
 
-	async get(key) {
-		return new Promise((resolve, reject) => {
-			this.client.get(key, (error, value) => {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(value);
-				}
-			});
-		});
-	}
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (error, value) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(value);
+                }
+            });
+        });
+    }
 
-	async set(key) {
-		this.client.set(key, value, 'EX', duration);
-	}
+    async set(key, value, duration) {
+        this.client.set(key, value, 'EX', duration);
+    }
 
-	async del(key) {
-		this.client.del(key);
-	}
+    async del(key) {
+        this.client.del(key);
+    }
 }
 
 const redisClient = new RedisClient();
