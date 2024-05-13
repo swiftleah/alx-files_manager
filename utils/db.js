54fd873cc4e51ad42e86f2357492e8ea2066a8d5
swiftleah <leahrosehenry@gmail.com>
2024-05-13 02:03:52 -0700

    Task 1 for now

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+import mongodb from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
