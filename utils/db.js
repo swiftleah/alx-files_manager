17fa3b2625c6dd3363806cd501ef8ba833da1c6a
swiftleah <leahrosehenry@gmail.com>
2024-05-13 02:20:47 -0700

    Task 1

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,18 @@
-import mongodb from 'mongodb';
+const { MongoClient } = require('mongodb');
 
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}/${database}`;
+    const url = `mongodb://${host}:${port}`;
 
-    this.client = new mongodb.MongoClient(url, { useUnifiedTopology: true });
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
     this.client.connect();
   }
 
   isAlive() {
-    return this.client.isConnected();
+	  return !!this.client && !!this.client.topology && this.client.topology.isConnected();
   }
 
   async nbUsers() {
@@ -25,5 +25,5 @@ class DBClient {
 
 }
 
-export const dbClient = new DBClient();
-export default dbClient;
+const dbClient = new DBClient();
+module.exports = dbClient;
